a547455f690ca5ce482e516f8940b75bebf070af
JanLuczewski <jan.luczewski@gmail.com>
2022-07-23 09:53:07 +0200

    argparser dodany

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import csv
 import xml.etree.ElementTree as ET
+import argparse
 
-def get_clientlist(csv_file):
-    csv_file = 'clients.csv'
-    with open(csv_file) as f:
+parser = argparse.ArgumentParser(description='robimy output.xml')
+parser.add_argument('plik_csv', metavar='path',type=str, help='podaj sciezke do pliku CSV')
+parser.add_argument('plik_xml',metavar='path',type=str,help=' podaj sciezke do pliku XML')
+args = parser.parse_args()
+input_path_csv = args.plik_csv
+input_path_xml = args.plik_xml
+
+
+
+# csv_file = 'clients.csv'
+def get_clientlist(input_path_csv):
+    with open(input_path_csv) as f:
         reader = csv.reader(f)
         clients_list = []
         for row in reader:
             clients_list.append(row[0])
         return clients_list
 
-xml_file = 'clients.xml'
-tree = ET.parse(xml_file)
-root = tree.getroot()
-print(root.find("clients"))
-for client in root.find("clients").findall("client"):
-    if client.attrib['name'] in get_clientlist(csv_file='clients.csv'):
-        if not client.find("messages"):
-            messages = ET.Element("messages")
-            client.append(messages)
-        messages = client.find("messages")
-        message = ET.Element("message")
-        message.text = 'huj huj'
-        messages.append(message)
-tree.write('output.xml')
+def get_outputxml(input_path_xml):
+    # xml_file = 'clients.xml'
+    tree = ET.parse(input_path_xml)
+    root = tree.getroot()
+    print(root.find("clients"))
+    for client in root.find("clients").findall("client"):
+        if client.attrib['name'] in get_clientlist(input_path_csv):
+            if client.find("messages") is None:
+                messages = ET.Element("messages")
+                client.append(messages)
+            messages = client.find("messages")
+            message = ET.Element("message")
+            message.text = 'huj huj'
+            messages.append(message)
+    tree.write('output.xml')
 
+wynik = get_outputxml('clients.xml')
+print(wynik)
 
