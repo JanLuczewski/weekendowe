4f932e50b0db713085662d8f5d65e2644ab027f7
JanLuczewski <jan.luczewski@gmail.com>
2022-07-23 11:20:50 +0200

    testy cd

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from main import get_clientlist
 
 class Get_Client_TestCase(unittest.TestCase):
@@ -6,3 +7,9 @@ class Get_Client_TestCase(unittest.TestCase):
         result = get_clientlist('clients.csv')
         expected = ['Test_1','Test_3']
         self.assertEqual(result,expected)
+    def test_empty_get_client_list(self):
+        path = os.path.join('test_files','test_csv.csv')
+        result = get_clientlist(path)
+        expected = []
+        self.assertEqual(result,expected)
+
